957324c367fe230e1e24fa71e05c4b8d712627f8
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2019-07-05 00:51:55 +0100

    Revert to main Monarch Biolink API server

--- translator_modules/core/__init__.py
+++ translator_modules/core/__init__.py
@@ -4,10 +4,10 @@ from abc import ABC
 class Config():
     def __init__(self):
         # Production endpoint
-        #self.biolink_api_endpoint = "http://api.monarchinitiative.org/api/"
+        self.biolink_api_endpoint = "http://api.monarchinitiative.org/api/"
 
         # Development endpoint - sometimes temporarily used
-        self.biolink_api_endpoint = "http://api-dev.monarchinitiative.org/api/"
+        #self.biolink_api_endpoint = "http://api-dev.monarchinitiative.org/api/"
 
     def getBiolinkApiEndpoint(self):
         return self.biolink_api_endpoint
