63028419b609e2611f3a38b16eef6d2722f96c6f
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2019-08-27 00:41:43 +0100

    small iteration on model

--- translator_modules/core/model.py
+++ translator_modules/core/model.py
@@ -14,10 +14,18 @@ definitions:
       source:
         type: string
         description: Module that produced the result list.
-      category:
+      input_category:
         type: string
         description: >-
-          Biolink Model concept category globally applicable to the results (e.g. 'gene')
+          Biolink Model concept category globally applicable to the module input data type (e.g. 'disease')
+      output_category:
+        type: string
+        description: >-
+          Biolink Model concept category globally applicable to the module output data type  (e.g. 'gene')
+      input_output_relationship:
+        type: string
+        description: >-
+          Biolink Model predicate tagging of the relationship
       attributes:
         type: array
         items:
@@ -63,13 +71,16 @@ definitions:
     type: object
     properties:
       # The namespaces will be a data type specific list
-      # Generally as found in the Biolink Model json-ld context file
-      # at https://github.com/biolink/biolink-model/blob/master/context.jsonld
+      # Generally as found in the Biolink Model json-ld context file at
+      # https://github.com/biolink/biolink-model/blob/master/context.jsonld
       # For example, for genes: NCBIGene, HGNC, ENSEMBL, MIM (actually missing from the context.jsonld?)
       xmlns:
         type: string
       id:
         type: string
+    required:
+      - xmlns
+      - id
 
   Attribute:
     type: object
@@ -84,3 +95,44 @@ definitions:
       - name
       - value
 """
+
+class ResultList():
+
+    def __init__(self, list_id, source=''):
+        self.list_id = list_id
+        self.source = source
+
+class Result():
+
+    def __init__(self, primary_id):
+        self.primary_id = primary_id
+
+    def getPrimaryId(self):
+        return self.primary_id
+
+    def setIdentifiers(self, identifiers):
+        self.identifiers = identifiers
+
+    def getIdentifiers(self):
+        return self.identifiers
+
+    def setAttributes(self, attributes):
+        self.attributes = attributes
+
+    def getAttributes(self):
+        return self.attributes
+
+
+class Identifier():
+
+    def __init__(self, xmlns, id):
+        self.xmlns = xmlns
+        self.id = id
+
+
+class Attribute():
+
+    def __init__(self, name, value, source=''):
+        self.name = name
+        self.value = value
+        self.source = source
