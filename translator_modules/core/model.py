5ef2be43403083c5756f913f95097726179b9087
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2019-08-27 22:35:26 +0100

    Convert Translator Modules interchange data model into Python 3.7 DataClass specification

--- translator_modules/core/model.py
+++ translator_modules/core/model.py
@@ -4,176 +4,147 @@ Working on an NCATS (Biolink Model) compliant data model and JSON standard...
 Partially inspired by the Indigo (Broad) team 'Gene Sharpener" data model for gene lists,
 plus a small bit of the ReasonerAPI nomenclature (here expressed in OpenAPI YAML=like notation)
 
-definitions:
-  ResultList:
-    type: object
-    properties:
-      list_id:
-        type: string
-        description: Id of the list of results. Generally an anonymous, globally unique UUID
-      source:
-        type: string
-        description: Module that produced the result list.
-      input_category:
-        type: string
-        description: >-
-          Biolink Model concept category globally applicable to the module input data type (e.g. 'disease')
-      output_category:
-        type: string
-        description: >-
-          Biolink Model concept category globally applicable to the module output data type  (e.g. 'gene')
-      relationship:
-        type: string
-        description: >-
-          Biolink Model predicate tagging of the relationship (relating to the "edge label" of a knowledge graph)
-      attributes:
-        type: array
-        items:
-          $ref: '#/definitions/Attribute'
-        description: Additional information and provenance about the result list.
-      results:
-        type: array
-        items:
-          $ref: '#/definitions/Result'
-        description: Members of the list of result entries.
-    required:
-      - list_id
-      - source
-      - entries
-
-  Result:
-    type: object
-    properties:
-      primary_id:
-        type: string
-        description: >-
-          Canonical Id of the result item.
-          For example, for genes, preferably HGNC id; can be NCBIGene or ENSEMBL id if HGNC id is not available.
-      identifiers:
-        type: array
-        items:
-          $ref: '#/definitions/Identifier'
-        description: >-
-          A standard list of equivalent identifiers for a given result (see below)
-      attributes:
-        type: array
-        items:
-          $ref: '#/definitions/Attribute'
-        description: >-
-          Additional information about the gene and provenance about result membership, possibly including various
-          scores associated with a given result.
-          For example, for genes, maybe use myGene.info to add the following attributes to every gene: 'gene_symbol',
-          'synonyms', 'gene_name',  and 'myGene.info id'. Multiple synonyms are separated by semicolons.
-    required:
-      - result_id
-
-  Identifier:
-    type: object
-    properties:
-      # The namespaces will be a data type specific list
-      # Generally as found in the Biolink Model json-ld context file at
-      # https://github.com/biolink/biolink-model/blob/master/context.jsonld
-      # For example, for genes: NCBIGene, HGNC, ENSEMBL, MIM (actually missing from the context.jsonld?)
-      xmlns:
-        type: string
-      id:
-        type: string
-    required:
-      - xmlns
-      - id
-
-  Attribute:
-    type: object
-    properties:
-      name:
-        type: string
-      value:
-        type: string
-      source:
-        type: string
-    required:
-      - name
-      - value
 """
 
-class ResultList():
-
-    def __init__(self, list_id, source='', input_category='', output_category='', relationship=''):
-        self.list_id = list_id
-        self.source = source
-        self.input_category = input_category
-        self.output_category = output_category
-        self.relationship = relationship
-        self.attributes = []
-        self.results = []
-
-    def getListId(self):
-        return self.list_id
-
-    def getSource(self):
-        return self.source
-
-    def setInputCategory(self, input_category):
-        self.input_category = input_category
-
-    def getInputCategory(self):
-        return self.input_category
-
-    def setOutputCategory(self, output_category):
-        self.output_category = output_category
-
-    def getOutputCategory(self):
-        return self.output_category
-
-    def setRelationship(self, relationship):
-        self.relationship=relationship
-
-    def setAttributes(self, attributes):
-        self.attributes.append(attributes)
-
-    def getAttributes(self):
-        return self.attributes
-
-    def setResults(self, results):
-        self.results.append(results)
-
-    def getAttributes(self):
-        return self.attributes
+from dataclasses import dataclass, field
+from typing import List
 
 
-class Result():
-
-    def __init__(self, primary_id):
-        self.primary_id = primary_id
-        self.identifiers = []
-        self.attributes = []
-
-    def getPrimaryId(self):
-        return self.primary_id
-
-    def setIdentifiers(self, identifiers):
-        self.identifiers.append(identifiers)
-
-    def getIdentifiers(self):
-        return self.identifiers
-
-    def setAttributes(self, attributes):
-        self.attributes.append(attributes)
-
-    def getAttributes(self):
-        return self.attributes
-
-
-class Identifier():
-
-    def __init__(self, xmlns, id):
-        self.xmlns = xmlns
-        self.id = id
-
-
+@dataclass
 class Attribute():
+    """
+    # Both 'Result' data objects and their aggregate collections
+    # may be documented with additional metadata attributes.
+      Attribute:
+        type: object
+        properties:
+          name:
+            type: string
+          value:
+            type: string
+          source:
+            type: string
+        required:
+          - name
+          - value
+    """
+    name: str
+    value: str
+    source: str = ''
 
-    def __init__(self, name, value, source=''):
-        self.name = name
-        self.value = value
-        self.source = source
+
+@dataclass
+class Identifier():
+    """
+    # Data objects in a 'Result' may be globally identified by different identifiers asserted by
+    # diverse bioinformatics authorities as indexed by their XML Namespace prefix
+
+      Identifier:
+        type: object
+        properties:
+          # The namespaces will be a data type specific list
+          # Generally as found in the Biolink Model json-ld context file at
+          # https://github.com/biolink/biolink-model/blob/master/context.jsonld
+          # For example, for genes: NCBIGene, HGNC, ENSEMBL, MIM (actually missing from the context.jsonld?)
+          xmlns:
+            type: string
+          id:
+            type: string
+        required:
+          - xmlns
+          - id
+    """
+    xmlns: str
+    id: str
+
+
+@dataclass
+class Result():
+    """
+    # A 'Result' is a single data record about a single entity
+    # in a possible list of results discovered by a computation.
+
+      Result:
+        type: object
+        properties:
+          primary_id:
+            type: string
+            description: >-
+              Canonical Id of the result item.
+              For example, for genes, preferably HGNC id; can be NCBIGene or ENSEMBL id if HGNC id is not available.
+          identifiers:
+            type: array
+            items:
+              $ref: '#/definitions/Identifier'
+            description: >-
+              A standard list of equivalent identifiers for a given result (see below)
+          attributes:
+            type: array
+            items:
+              $ref: '#/definitions/Attribute'
+            description: >-
+              Additional information about the gene and provenance about result membership, possibly including various
+              scores associated with a given result.
+              For example, for genes, maybe use myGene.info to add the following attributes to every gene: 'gene_symbol',
+              'synonyms', 'gene_name',  and 'myGene.info id'. Multiple synonyms are separated by semicolons.
+        required:
+          - result_id
+    """
+    primary_id: str
+    identifiers: List[Identifier] = field(default_factory=list)
+    attributes: List[Attribute] = field(default_factory=list)
+
+
+@dataclass
+class ResultList():
+    """
+    # A 'ResultList' is a (possibly ordered) documented collection of
+    # 'Result' data objects returned by a computation.
+    #
+    # Such a list of results is documented with mandatory metadata (e.g. Biolink Model
+    # characterization of the data types returned, and their inferred relationships to input data)
+    # and possibly with optional attributes documenting provenance and global characteristics of the results.
+
+      ResultList:
+        type: object
+        properties:
+          list_id:
+            type: string
+            description: Id of the list of results. Generally an anonymous, globally unique UUID
+          source:
+            type: string
+            description: Module that produced the result list.
+          input_category:
+            type: string
+            description: >-
+              Biolink Model concept category globally applicable to the module input data type (e.g. 'disease')
+          output_category:
+            type: string
+            description: >-
+              Biolink Model concept category globally applicable to the module output data type  (e.g. 'gene')
+          relationship:
+            type: string
+            description: >-
+              Biolink Model predicate tagging of the relationship (relating to the "edge label" of a knowledge graph)
+          attributes:
+            type: array
+            items:
+              $ref: '#/definitions/Attribute'
+            description: Additional information and provenance about the result list.
+          results:
+            type: array
+            items:
+              $ref: '#/definitions/Result'
+            description: Members of the list of result entries.
+        required:
+          - list_id
+          - source
+          - entries
+    """
+    list_id: str
+    source: str = ''
+    input_category: str = ''
+    output_category: str = ''
+    relationship: str = ''
+    attributes: List[Attribute] = field(default_factory=list)
+    results: List[Result] = field(default_factory=list)
