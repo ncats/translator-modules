729f1648b1c770429d82c9f19904b2e64a4fa362
Richard Bruskiewich <richard@starinformatics.com>
2019-10-08 14:06:04 -0700

    Generated server stubs for ontology Jaccard similarity service

---
+++ ncats/translator/ontology/server/openapi_server/controllers/public_controller.py
@@ -0,0 +1,35 @@
+import connexion
+import six
+
+from openapi_server.model.computation_identifier import ComputationIdentifier  # noqa: E501
+from openapi_server.model.computation_input import ComputationInput  # noqa: E501
+from openapi_server.model.results import Results  # noqa: E501
+from openapi_server import util
+
+
+def compute_jaccard(computation_input=None):  # noqa: E501
+    """post a list of input genes and initiate a Jaccard similarity computation
+
+    Post a list of input genes and initiate a Jaccard similarity computation  # noqa: E501
+
+    :param computation_input: List of input genes upon which to compute Jaccard similarity 
+    :type computation_input: dict | bytes
+
+    :rtype: ComputationIdentifier
+    """
+    if connexion.request.is_json:
+        computation_input = ComputationInput.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def get_results(compute_id):  # noqa: E501
+    """Retrieves a list of similarity results when ready 
+
+    Retrieves a list of similarity results when obtained by a Jaccard similarity of a posted list of input genes  # noqa: E501
+
+    :param compute_id: Computational Identifier UUID returned from a submitted Jaccard similarity computation request upon a posted list of input genes 
+    :type compute_id: str
+
+    :rtype: Results
+    """
+    return 'do some magic!'
---
+++ ncats/translator/ontology/server/openapi_server/test/test_public_controller.py
@@ -0,0 +1,68 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+import unittest
+
+from flask import json
+from six import BytesIO
+
+from openapi_server.model.computation_identifier import ComputationIdentifier  # noqa: E501
+from openapi_server.model.computation_input import ComputationInput  # noqa: E501
+from openapi_server.model.results import Results  # noqa: E501
+from openapi_server.test import BaseTestCase
+
+
+class TestPublicController(BaseTestCase):
+    """PublicController integration test stubs"""
+
+    def test_compute_jaccard(self):
+        """Test case for compute_jaccard
+
+        post a list of input genes and initiate a Jaccard similarity computation
+        """
+        computation_input = {
+  "input_genes" : [ {
+    "sim_input_curie" : "P38398",
+    "input_symbol'" : "BRCA1",
+    "input_id" : "HGNC:1100"
+  }, {
+    "sim_input_curie" : "P38398",
+    "input_symbol'" : "BRCA1",
+    "input_id" : "HGNC:1100"
+  } ],
+  "taxon" : "human",
+  "ontology" : "ontology"
+}
+        headers = { 
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+        response = self.client.open(
+            '/compute_jaccard',
+            method='POST',
+            headers=headers,
+            data=json.dumps(computation_input),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_results(self):
+        """Test case for get_results
+
+        Retrieves a list of similarity results when ready 
+        """
+        query_string = [('compute_id', 'compute_id_example')]
+        headers = { 
+            'Accept': 'application/json',
+        }
+        response = self.client.open(
+            '/results',
+            method='GET',
+            headers=headers,
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    unittest.main()
