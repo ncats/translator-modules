f4de063db8888e9116ca557872933c5fff1a524d
Richard Bruskiewich <richard@starinformatics.com>
2019-10-21 14:05:09 -0700

    Initial iteration on wiring up Jaccard with modules

--- ncats/translator/core/generic_similarity.py
+++ ncats/translator/core/generic_similarity.py
@@ -3,13 +3,13 @@
 from typing import List, Tuple
 
 import pandas as pd
-from ontobio.assocmodel import AssociationSet
 
-#
-# We override the direct Ontobio version of the Jaccard_Similarity
-# function below, to return shared ontology term annotation
-#
-# from ontobio.analysis.semsim import jaccard_similarity
+from ncats.translator.ontology.client.openapi_client.api.public_api import PublicApi
+from ncats.translator.ontology.client.openapi_client.configuration import Configuration
+from ncats.translator.ontology.client.openapi_client.api_client import ApiClient
+from ncats.translator.ontology.client.openapi_client.model.computation_input import ComputationInput
+from ncats.translator.ontology.client.openapi_client.model.computation_identifier import ComputationIdentifier
+from ncats.translator.ontology.client.openapi_client.exceptions import ApiException
 
 
 class GenericSimilarity(object):
@@ -18,9 +18,41 @@ class GenericSimilarity(object):
         self.ont = ''
         self.taxon = ''
 
+        configuration = Configuration()
+
+        # Defining host is optional and defaults to http://0.0.0.0:8082
+        configuration.host = "http://0.0.0.0:8082"
+
+        # Create an instance of the API class
+        self.client = PublicApi(ApiClient(configuration))
+
     def compute_jaccard(self, input_genes: List[dict], lower_bound: float = 0.7) -> List[dict]:
+
+        computation_input = ComputationInput(
+            ontology=self.ont,
+            taxon=self.taxon,
+            lower_bound=lower_bound,
+            input_genes=""
+        )
+
+        computation_id: ComputationIdentifier
+        try:
+            # post a list of input genes and initiate a Jaccard similarity computation
+            computation_id = self.client.compute_jaccard(computation_input=computation_input)
+        except ApiException as e:
+            print("Exception when calling PublicApi->compute_jaccard: %s\n" % e)
+            return []
+
+        try:
+            # attempt to retrieve the results
+            computation_id = self.client.get_results(computation_id.uuid)
+            computation_id
+        except ApiException as e:
+            print("Exception when calling PublicApi->get_results: %s\n" % e)
+            return []
+
         similarities = []
-        # need to call the remote service here, tagged  with self.taxon and self.ont to be used
+
         return similarities
 
     @staticmethod
